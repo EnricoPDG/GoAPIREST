621902439b2b36d9b1790bff413e36d3fac97711
EnricoPDG <ricopapsch@hotmail.com>
2022-05-31 14:53:54 -0300

    Conecting to postgree database, GORM

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/EnricoPDG/GoAPIREST/models"
+	"github.com/EnricoPDG/GoAPIREST/database"
 	"github.com/EnricoPDG/GoAPIREST/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1,Nome:"Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando servidor Rest Go")
 	routes.HandleRequest()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,6 @@ func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
-	r.HandleFunc("/api/personalidades{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
