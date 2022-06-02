e6e2fef7cf348bb8d1f499afb62bd9458200a512
EnricoPDG <ricopapsch@hotmail.com>
2022-06-02 15:39:09 -0300

    Adding middleware and conect to front-end

--- routes/routes.go
+++ routes/routes.go
@@ -3,14 +3,21 @@ package routes
 import (
 	"log"
 	"net/http"
+
 	"github.com/EnricoPDG/GoAPIREST/controllers"
+	"github.com/EnricoPDG/GoAPIREST/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
